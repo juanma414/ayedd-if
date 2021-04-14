352b4e38a4feeb800950beaa160cfa06f9742f23
juan manuel tarrago <juanmanueltarrago@gmail.com>
2021-04-14 09:43:40 -0300

    mi primer commit

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+A = float(input("ingrese el primer numero:"))
+B = float(input("ingrese el segundo numero:"))
+if A>B:
+	mayor=A
+else: 
+	mayor=B
+print("el mayor es:",mayor)
---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+num=float(input("ingrese el número"))
+if (num>0 ):
+	print("el número es positivo")
+elif (num == 0 ):
+	print ("el número es cero")
+else: 
+	print("el número es negativo")
