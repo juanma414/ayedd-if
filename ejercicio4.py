1967c3a9e73de15a9fde1d0fa95b84808fea786b
juan manuel tarrago <juanmanueltarrago@gmail.com>
2021-04-15 20:43:23 -0300

    modifico el 4

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,9 @@
-#este va a ser el ejercicio 4 
-#mnodigico para hacer algo
-#esto hay que agregarlo
-print("hola mundo")
-print("ahora quiero ver este cambio")
+#ingresando 3 numeros determinar si estan ordenados en orden creciente
+a == (input("ingrese el primer número:")
+b == (input("ingrese el segundo número:")
+c == (input("ingrese el tercer número:")
+
+if a < b < c:
+	 print("se ingresaron en orden creciente")
+else:
+	print("no se ingresaron en orden creciente")
