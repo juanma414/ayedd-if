b8d20cf45d0976daa5af07d5a373d12507f62f72
juan manuel tarrago <juanmanueltarrago@gmail.com>
2021-04-14 21:53:21 -0300

    ahora el ejercicio 3 esta hecho

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+a = float(input("ingrese el primer ángulo:"))
+b = float(input("ingrese el segundo ángulo:"))
+c = float(input("ingrese el tercer ángulo:"))
+if (a==90 or b==90 or c==90):
+	print("es un triángulo rectángulo")
+else:
+	print("no es un triángulo rectángulo")
